be5c5910f6730f6fa61bda0d5ec963da3faec164
Alexander Schelchere <ascellie@gmail.com>
2019-06-12 10:26:33 -0400

    COMPLETE - all deliverables met & All tests pass when running tools/console.rb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -16,22 +16,22 @@ class Restaurant
   def customers
     # returns a unique list of all customers who have 
     # reviewed a particular restaurant.
-
+    reviews.map { |review| review.customer }
   end
 
   def reviews
     # returns an array of all reviews for that restaurant
-
+    Review.all.select { |review| review.restaurant == self }
   end
 
   def average_star_rating
     # returns the average star rating for a restaurant based on its reviews
-
+    reviews.map { |review| review.rating }.sum / reviews.count
   end
 
   def longest_review
     # returns the longest review content for a given restaurant
-
+    reviews.map { |review| review.content }.max_by(&:length)
   end
 
 end
