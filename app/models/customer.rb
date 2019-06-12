ba797fa6dfc0d42d712ce0eda5547a4d914e0f8b
github username <github email address>
2019-06-12 10:50:59 -0400

    YAS

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,43 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    self.class.all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def num_reviews
+    reviews.count
+  end
+
+  def restaurants
+    reviews.map { |review| review.restaurant }.uniq
+  end
+
+  def self.find_by_name(name)
+    # Itirate through Reviews
+    # Split my
+  end
+
 end
