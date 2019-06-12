34ca7e0fcd81aad31765dc270004392475d46766
hamzaja <hamzajaved71@gmail.com>
2019-06-12 10:45:45 -0400

    code challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,42 @@
 class Customer
-  attr_reader :first_name, :last_name
+  @@all = []
+  attr_reader :first_name, :last_name ,:review , :restaurant
 
-  def initialize(first_name, last_name)
+  def initialize(first_name, last_name , review , restaurant )
     @first_name = first_name
     @last_name  = last_name
+    @review = review
+    @restaurant = restaurant
+    @@all<<self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+
   end
-end
+
+  def num_reviews
+    @@all.map{|customer| customer.review}.count
+  end
+
+  def restaurants
+    @@all.select{|customer| customer.restaurant }.uniq
+    
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    ####
+  end
+  def self.find_all_by_first_name(name)
+    #all customer with first name 
+  end
+  def self.all_names
+    #full name
+  end
+
+
+end 
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,27 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+customer1 =Customer.new("first1 ", "last2" , "fisrtttttt" , "resturant1")
+customer2 =Customer.new("fisrt2", "last3" , "f2nddddt" , "resturant2")
+customer3 =Customer.new("first3", "last3" , "3rdddddd" , "resturant3")
+
+
+restaurant1=  Restaurant.new("1111111")
+restaurant2=  Restaurant.new("2222222")
+restaurant3=  Restaurant.new("3333333")
+
+review1 = Review.new(customer1, restaurant1 ,3)
+review2 = Review.new(customer2, restaurant3 ,2)
+review3 = Review.new(customer3, restaurant2 ,4)
+review4 = Review.new(customer3, restaurant2 ,5)
+review5 = Review.new(customer3, restaurant2 ,3)
+review6 = Review.new(customer2, restaurant3 ,2)
+review7 = Review.new(customer1, restaurant1 ,3)
+
+
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
