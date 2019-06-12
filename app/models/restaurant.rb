03fcc95fb267b8ea2cff4bd2d836b303a0a0bf0d
github username <github email address>
2019-06-12 10:42:35 -0400

    Solution

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,61 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  # Helper method; returns reviews associated with a particular restaurant
+  def reviews
+    Review.all.select { |review| review.restaurant == self }
+  end
+
+  def customers
+    reviews.map { |review| review.customer }.uniq
+  end
+
+  def average_star_rating
+    reviews.map { |review| review.rating }.sum / reviews.length
+  end
+
+  def review_content
+    reviews.collect { |review| review.content }
+  end
+
+  def review_content_lengths
+    reviews.map { |review| review.content.length }
+  end
+
+  def longest_review
+    max_length = review_content_lengths.max
+    review_content.select { |content| content.length == max_length }
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |restaurant| restaurant.name == name }
+  end
 end
+
+# Review (customer, restaurant, rating, content)
+
+# Restaurant#customers
+# Returns a unique list of all customers who have reviewed a particular restaurant.
+
+# Restaurant#reviews
+# returns an array of all reviews for that restaurant
+
+# Restaurant#average_star_rating
+# returns the average star rating for a restaurant based on its reviews
+
+# Restaurant#longest_review
+# returns the longest review content for a given restaurant
+
+# Restaurant.find_by_name(name)
+# given a string of restaurant name, returns the first restaurant that matches
--- tools/console.rb
+++ tools/console.rb
@@ -1,11 +1,31 @@
-require_relative '../config/environment.rb'
+require_relative "../config/environment.rb"
+require "pry"
 
 def reload
-  load 'config/environment.rb'
+  load "config/environment.rb"
 end
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+
+# Customer => (first_name, last_name)
+cust1 = Customer.new("John", "Doe")
+cust2 = Customer.new("Albert", "Eisnstien")
+cust3 = Customer.new("Karl", "Markx")
+cust4 = Customer.new("Bill", "Clinton")
+
+# Resturant => (name)
+rest1 = Restaurant.new("La Habana")
+rest2 = Restaurant.new("La Mesa")
+rest3 = Restaurant.new("La Isla")
+rest4 = Restaurant.new("La Terra")
+
+# Review => (customer, restaurant, rating, content)
+rev1 = Review.new(cust1, rest1, 3, "Good Food")
+rev2 = Review.new(cust2, rest2, 2, "So So Food")
+rev3 = Review.new(cust3, rest3, 5, "Gooood Foo")
+rev4 = Review.new(cust4, rest4, 1, "Bad Food")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
